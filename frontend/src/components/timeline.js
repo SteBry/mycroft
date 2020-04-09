ff7ed7b46859be970a6b60fc42fd8f5af7fe46fd
Erik Sellén <erik_44a@hotmail.com>
2020-04-09 18:55:15 +0200

    feat: add css to timeline

--- frontend/src/components/timeline.js
+++ frontend/src/components/timeline.js
@@ -5,35 +5,33 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import { logScaling } from '../stateTimeline';
 
+import styles from './timeline.module.css';
 
 /* -- Timeline -- */
 class Timeline extends Component {
 
     logThis (a) {
         //this.props.funcDummy();
-        if (a === 1) {
-            console.log('1');
-        } else if (a === 2) {
-            console.log('2');
-        } else {
-            console.log('3');
-        }
+        console.log(a);
     }
 
     render() { 
         return (
-            <React.Fragment>
+            <div className={styles.main}>
                 <p> Timeline Component </p>
-                <DropdownButton title={"Time scale"}>
+                <DropdownButton className={styles.dropdown} title={"Time scale"}>
                     {["12 Hours", "24 Hours", "36 Hours"].map((i) => {
                         return (
-                            <Dropdown.Item onClick={(a) => this.logThis(parseInt(1))}>
+                            <Dropdown.Item onClick={(a) => this.logThis(i.split(' ')[0])}>
                                 {i}
                             </Dropdown.Item>
                         );
                     })}
                 </DropdownButton>
-            </React.Fragment>
+                <div className ={styles.sliderbox}>
+                    <p> Hej </p>
+                </div>
+            </div>
         );
     }
 }
